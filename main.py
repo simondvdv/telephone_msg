1080da123a6309a41f6c318d7973d4443d539957
simondvdv <simondv4@gmail.com>
2024-08-05 17:45:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+DICT = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+        '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+
+
+def number_input():
+    try:
+        digits = input('Vvedite chislo is cifr "23456789", 9999 - max\n')
+        if digits == '':
+            return ''
+        if one_and_zero_check(digits):
+            return number_input()
+    except ValueError:
+        print('Vvedi norm chislo!')
+        return number_input()
+    return digits
+
+
+def one_and_zero_check(digits):
+    if int(digits) > 9999:
+        print('Vvedi chislo <9999')
+        return True
+    for i in str(digits):
+        if i == '1' or i == '0':
+            print('Vvedite chislo bez 1 i 0')
+            return True
+    return False
+
+
+def list_maker():
+    digit = number_input()
+    if len(digit) == 0:
+        return []
+    answer_list = ['']
+    len_list = 1
+    for i in digit:
+        len_list *= len(DICT[i])
+    print(len_list)
+    answer_list = answer_list * len_list
+    
+    return answer_list
+
+
+print(list_maker())
+
